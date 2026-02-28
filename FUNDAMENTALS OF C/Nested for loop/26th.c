2f15883083541e68fdf165f0544b08d4aab69c54
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-02-28 20:50:19 +0530

    Day 28

---
+++ FUNDAMENTALS OF C/Nested for loop/26th.c
@@ -0,0 +1,20 @@
+///question no 26
+/*
+    1
+    1   0
+    1   0   1
+    1   0   1   0
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j%2);
+        }
+        printf("\n");
+    }
+}
