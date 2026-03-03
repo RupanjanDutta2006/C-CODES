c47c3a1ad3100d0ff53aeb0afb82a2bdb03a64e9
RupanjanDutta2006 <rupanjandutta06@gmail.com>
2026-03-03 23:37:20 +0530

    Day 29
    Add a new C program that reads a square matrix from user input, prints the full matrix, and prints its lower-left triangular part. The program prompts for row/column counts and element values, validates that the matrix is square (r==c), and prints an error message if not. File added: FUNDAMENTALS OF C/2-D Matrix/Lower-Left-Triangle.c

---
+++ FUNDAMENTALS OF C/2-D Matrix/Lower-Left-Triangle.c
@@ -0,0 +1,45 @@
+//Lower Left triangle
+/*
+    1
+    4   5
+    7   8   9
+*/
+#include<stdio.h>
+void main()
+{
+    int i,j,r,c;
+    printf("Enter the Row and Column No = ");
+    scanf("%d%d",&r,&c);
+    if(r==c)
+    {
+        int a[r][c];
+        printf("Enter the Elements for %dX%d matrix= \n",r,c);
+        //input
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                printf("Enter the Element for (%d,%d) index = ",i,j);
+                scanf("%d",&a[i][j]);
+            }
+        }
+        //output
+        printf("The Matrix is = \n");
+        for(i=0;i<r;i++)
+        {
+                for(j=0;j<c;j++)
+                printf("%d ",a[i][j]);
+                printf("\n");
+        }
+        //Lower Left triangle
+        printf("Lower Left Triangle\n");
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+                (i>=j)?printf("%d ",a[i][j]):printf("  ");
+            printf("\n");
+        }
+    }
+    else
+        printf("1st & 2nd matrix's row and Column no are not same,so not possible");
+}
